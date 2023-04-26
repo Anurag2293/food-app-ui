d471a3e4e26a306d6a09c11b280481acf788d703
Anurag Dhote <anuragdhote392@gmail.com>
2023-04-26 19:38:20 +0530

    Fixed the hero part

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import Carousel from 'react-material-ui-carousel'
-import { Paper, Button, Typography } from '@mui/material'
+import { Paper, Typography } from '@mui/material'
+
+import backgroundOne from './assets/background1.avif';
+import backgroundTwo from './assets/background-2.jpg';
 
 function Item(props)
 {
     return (
         <Paper
-            sx={{ bgcolor: 'red', height: '40vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}
+            elevation={0}
+            sx={{ 
+                width: '100%',
+                height: '40vw', 
+                display: 'flex', 
+                flexDirection: 'column', 
+                justifyContent: 'center', 
+                alignItems: 'center',
+                background: `url(${props.item.imageSrc})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+            }}
         >
-            <Typography variant='h2'>{props.item.name}</Typography>
-            <Typography>{props.item.description}</Typography>
-
-            <Button variant='contained' className="CheckButton">
-                Check it out!
-            </Button>
+            <Typography variant='h1' 
+                sx={{
+                    fontWeight: '900',
+                    color: '#fff'
+                }}
+            >{props.item.name}</Typography>
+            <Typography variant='h5'
+                sx={{ 
+                    color: '#fff',
+                    fontFamily: `Okra, Helvetica, sans-serif`
+                }}
+            >{props.item.description}</Typography>
         </Paper>
     )
 }
@@ -22,22 +42,25 @@ const Hero = () => {
 
     const items = [
         {
-            name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
+            name: "Food.com",
+            description: "Find the best restaurants, cafes and bars in India!",
+            imageSrc: backgroundOne
         },
-        {
-            name: "Random Name #2",
-            description: "Hello World!"
-        }
+        // {
+        //     name: "Instant Delivery",
+        //     description: "Get items delivered in record time!",
+        //     imageSrc: backgroundTwo
+        // }
     ]
 
     return (
         <Paper
+            elevation={0}
             sx={{
-                width: '80%',
-                // mx: 'auto',
-                bgcolor: 'red',
-                borderRadius: '40px'
+                width: '100%',
+                borderRadius: 0,
+                p: 0,
+                m: 0
             }}
         >
 
@@ -48,7 +71,6 @@ const Hero = () => {
                     style: {
                         display: 'none'
                     }
-
                 }}
             >
                 {items.map( (item, i) => 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,6 @@ const Home = () => {
             <Paper
                 sx={{
                     width: '100%',
-                    height: '100vh',
                     display: 'flex',
                     flexFlow: 'column',
                     alignItems: 'center',
